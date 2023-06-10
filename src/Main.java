808866620c03e11878e8427b00cd2436a7a20cb2
Olena573 <linboykomail@gmail.com>
2023-06-10 20:33:11 +0300

    This is a beginned homework

---
+++ src/Main.java
@@ -0,0 +1,127 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String args[]) {
+        //here is a list of methods for each task from a homework
+        numeric_and_for();
+
+        for_and_multiplying();
+
+        simple_while_cycle();
+
+        simple_do_while_cycle();
+
+        two_dimensional_array();
+
+    }
+
+     static void numeric_and_for(){
+        /* here I will use "for" cycle for filling the int massive
+         * of integers with data and printing them out, also with the "for"
+         * also, a "scanner" class will be used
+         */
+      int [] array1 = new [50];
+     //here the "for" cycle is used for filling the one-dimensional massive with data;
+     for (int j = 0; j<50; j++){
+     array1[j] = +20
+     }
+
+     //here the "For" cycle is used for printing out the data of the massive;
+     for (int i = 0; i<50; i++){
+     System.out.println(array1[i]);
+
+     }
+     }
+
+     static void for_and_multiplying(){
+        //here the "for" cycles should be used for printing the backward multiplying table
+     int a = 2;
+     int b = 9;
+     for (int i = 0; i<10; i++){
+         for (int j = 0; j<20; j++){
+            System.out.println();
+         }
+
+     }
+
+
+
+
+
+     }
+
+    static void simple_while_cycle() {
+        //here I will bring an example of the usage of the "While" cycle.
+        //First of all, a massive of String values. I've decided to use some text old-school smileys.
+        String[] smileys = new String[]{
+                "=-)", "=-(", "=-0", "=-P", "=-3",
+                "B-)", "B-(", ">=-(", "B-P", ":-)", ";-)", "=-/",
+                "^_^", "*_*", "T_T", "X-(", "O_O", "=-D", "Q_Q", "8-)"};
+        int i = 0;
+
+        // Here I will use a "While" cycle.
+        while (i < 20) {
+            i++;
+            System.out.println(smileys[i]);
+        }
+    }
+
+
+    static void simple_do_while_cycle() {
+        //here I will bring an example of the simple do-while cycle
+
+        int[] integer_massive = [4, 5, 7, 3, 8, 21, 32, 56, 78]
+        int j = 10
+        do {
+            System.out.println("Something beautiful comes this way!" + integer_massive);
+            j--;
+        } while (j <= 32);
+
+
+    }
+}
+    static void two_dimensional_array(){
+    //here I will work with 2-dimensional array of integers and inner cycles
+    int some_array2[][] = new [45][23]
+
+
+    for (int i = 0; i<50; i++){
+           for (int j = 0; j++; j<35){
+     }
+    }
+
+    }
+* }
+* //until I am not done with this part of my homework, there is no time for the other part.
+*
+*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+* public static void main(String[] args) {
+*        Press Alt+Enter with your caret at the highlighted text to see how
+*       / IntelliJ IDEA suggests fixing it.
+*       System.out.printf("Hello and welcome!");
+*
+*       Press Shift+F10 or click the green arrow button in the gutter to run the code.
+*       for (int i = 1; i <= 5; i++) {
+*           Press Shift+F9 to start debugging your code. We have set one breakpoint
+*
+*          for you, but you can always add more by pressing Ctrl+F8. *           System.out.println("i = " + i);
+*       }
+*   }
+* }
+**/
+
